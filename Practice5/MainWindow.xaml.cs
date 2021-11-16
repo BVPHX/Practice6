aaa38f692755fb4db0a4b03d63dbe5c7ab1bb25e
mpiece228@gmail.com <mpiece228@gmail.com>
2021-11-16 23:29:39 +0300

    Добавьте файлы проекта.

---
+++ Practice5/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Triad;
+
+namespace Practice5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {/*Triads.Compare(firstTriad, secondTriad);*/
+        Triads firstTriad = new Triads();
+        Triads secondTriad = new Triads();
+        public MainWindow()
+        {
+            InitializeComponent();
+            firstTriadBox.Text = firstTriad.First.ToString() + " " + firstTriad.Second.ToString() + " " + firstTriad.Third.ToString();
+            secondTriadBox.Text = secondTriad.First.ToString() + " " + secondTriad.Second.ToString() + " " + secondTriad.Third.ToString();
+
+        }
+
+        private void FirstTriadIncrease(object sender, RoutedEventArgs e)
+        {
+            firstTriad.IncreaseTriadValuesTo10();
+            firstTriadBox.Text = firstTriad.First.ToString() + " " + firstTriad.Second.ToString() + " " + firstTriad.Third.ToString();
+        }
+
+        private void SecondTriadIncrease(object sender, RoutedEventArgs e)
+        {
+            secondTriad.IncreaseTriadValuesTo10();
+            secondTriadBox.Text = secondTriad.First.ToString() + " " + secondTriad.Second.ToString() + " " + secondTriad.Third.ToString();
+        }
+
+        private void CompareButton(object sender, RoutedEventArgs e)
+        {
+            if (firstTriad > secondTriad) MessageBox.Show("Значения первой триады больше значению второй");
+            else MessageBox.Show("Некоторые значения первой триады меньше значений второй триады");
+        }
+    }
+}
---
+++ Triad/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Triad
+{
+    public class Triads
+    {
+        int _first;
+        int _second;
+        int _third;
+
+        public Triads()
+        {
+            Random rnd = new Random();
+            _first = rnd.Next(0, 10);
+            _second = rnd.Next(0, 10);
+            _third = rnd.Next(0, 10);
+        }
+
+        public int First { get { return _first; } set { _first = value; } }
+        public int Second { get { return _second; } set { _second = value; } }
+        public int Third { get { return _third; } set { _third = value; } }
+
+        public static bool operator >(Triads firstTriad,Triads secondTriad)
+        {
+           if (firstTriad.First > secondTriad.First && firstTriad.Second > secondTriad.Second && firstTriad.Third > secondTriad.Third) return true;
+            else return false;
+        }
+        public static bool operator <(Triads firstTriad, Triads secondTriad)
+        {
+            if (firstTriad.First < secondTriad.First && firstTriad.Second < secondTriad.Second && firstTriad.Third < secondTriad.Third) return true;
+            else return false;
+        }
+
+        public void IncreaseTriadValuesTo10()
+        {
+            _first += 10;
+            _second += 10;
+            _third += 10;
+        }
+    }
+   
+}
